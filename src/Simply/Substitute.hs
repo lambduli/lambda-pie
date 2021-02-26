cfe25b52c4cb48c7805fd310b79f239f14840b83
Taskkill <johny95wil@gmail.com>
2021-02-26 17:55:00 +0100

    refactor and remove a need for the explicit alpha and beta reductions

---
+++ src/Simply/Substitute.hs
@@ -0,0 +1,22 @@
+module Simply.Substitute where
+
+import Simply.AST
+
+
+subst'infer :: Int -> Term'Infer -> Term'Infer -> Term'Infer
+subst'infer level rep (exp ::: type')
+  = subst'check level rep exp ::: type'
+subst'infer level rep (Bound indx name)
+  | level == indx = rep
+  | otherwise  = Bound indx name
+subst'infer level rep (Free name)
+  = Free name
+subst'infer level rep (left :@: right)
+  = subst'infer level rep left :@: subst'check level rep right
+
+
+subst'check :: Int -> Term'Infer -> Term'Check -> Term'Check
+subst'check level rep (Inf exp)
+  = Inf (subst'infer level rep exp)
+subst'check level rep (Lam par body)
+  = Lam par (subst'check (level + 1) rep body)
--- src/Simply/TypeChecker.hs
+++ src/Simply/TypeChecker.hs
@@ -6,49 +6,57 @@ import Simply.Context
 import Simply.Kind
 import Simply.Type
 import Simply.AST
+import Simply.Name
+import Simply.Substitute
+
 
 type Result a = Either String a
 
+
 throwError :: String -> Result a
-throwError err =
-  Left err
+throwError = Left
 
 
 kind'check :: Context -> Type -> Kind -> Result ()
-kind'check context (TVar name) Star =
+kind'check context (TFree name) Star =
   case lookup name context of
     Just (HasKind Star) -> return ()
-    Just _ -> throwError $ "A type var " ++ name ++ " has not kind of a *."
-    Nothing -> throwError $ "Unknown type var identifier " ++ name ++ "."
+    Just _ -> throwError $ "A type var " ++ show name ++ " has not kind of a *."
+    Nothing -> throwError $ "Unknown type var identifier " ++ show name ++ "."
 kind'check context (left't :-> right't) Star = do
   kind'check context left't Star
   kind'check context right't Star
 
 
-type'infer :: Context -> Term'Infer -> Result Type
-type'infer context (e ::: type') = do
+type'infer'0 :: Context -> Term'Infer -> Result Type
+type'infer'0 = type'infer 0
+
+
+type'infer :: Int -> Context -> Term'Infer -> Result Type
+type'infer level context (e ::: type') = do
   kind'check context type' Star
-  type'check context e type'
+  type'check level context e type'
   return type'
-type'infer context (Var name) = do
+type'infer level context (Free name) = do
   case lookup name context of
     Just (HasType type') -> return type'
-    Just _ -> throwError $ "Type error for " ++ name ++ "."
+    Just _ -> throwError $ "Type error for " ++ show name ++ "."
     Nothing -> throwError "Unknown identifier."
-type'infer context (left :@: right) = do
-  left't <- type'infer context left
+type'infer level context (left :@: right) = do
+  left't <- type'infer level context left
   case left't of
     in't :-> out't -> do
-      type'check context right in't
+      type'check level context right in't
       return out't
     _ -> throwError "Type error: illegal application."
 
 
-type'check :: Context -> Term'Check -> Type -> Result ()
-type'check context (Inf e) type' = do
-  e't <- type'infer context e
+type'check :: Int -> Context -> Term'Check -> Type -> Result ()
+type'check level context (Inf e) type' = do
+  e't <- type'infer level context e
   unless (type' == e't) (throwError "Type mismatch.")
-type'check context (Lam par body) (in't :-> out't) = do
-  type'check ((par, HasType in't) : context ) body out't
-type'check _ _ _ =
+type'check level context (Lam par body) (in't :-> out't) = do
+  type'check (level + 1) ((Local level par, HasType in't) : context)
+            (subst'check 0 (Free (Local level par)) body) out't
+type'check _ _ _ _ =
   throwError "Type mismatch."
