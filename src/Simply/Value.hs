be0310c783716b1010e09cdc1c0f1433ff16be43
Taskkill <johny95wil@gmail.com>
2021-02-26 15:25:00 +0100

    use the new binding + close a lambda

--- src/Simply/Value.hs
+++ src/Simply/Value.hs
@@ -1,8 +1,13 @@
 module Simply.Value where
 
+import Simply.AST
+
+
+type Env = [Value]
+
 
 data Value
-  = Lam String Value
-  | Var String
+  = Lam String Term'Check Env
+  | Free String
   | App Value Value
   deriving (Show)
