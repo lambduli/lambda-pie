034ade7171596e1e41e60f5fa0f73d479cf1c69d
Taskkill <johny95wil@gmail.com>
2021-03-03 13:59:50 +0100

    add my own show implementation

--- src/Simply/Type.hs
+++ src/Simply/Type.hs
@@ -6,4 +6,11 @@ import Simply.Name ( Name )
 data Type
   = TFree Name
   | Type :-> Type
-  deriving (Show, Eq)
+  deriving (Eq)
+
+
+instance Show Type where
+  show (TFree name)
+    = show name
+  show (from :-> to)
+    = "(" ++ show from ++ " -> " ++ show to ++ ")"
