6d1703854845c4e407be272bb4e5f2b299336597
Taskkill <johny95wil@gmail.com>
2021-03-03 13:59:22 +0100

    add my own show implementation

--- src/Simply/Name.hs
+++ src/Simply/Name.hs
@@ -4,4 +4,11 @@ module Simply.Name where
 data Name
   = Global String
   | Local Int String
-  deriving (Show, Eq)
+  deriving (Eq)
+
+
+instance Show Name where
+  show (Global name)
+    = name
+  show (Local ind name)
+    = name ++ show ind
