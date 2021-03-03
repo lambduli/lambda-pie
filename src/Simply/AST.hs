7c70f476251a8460d55a4ae2224d5f3c7a6789a9
Taskkill <johny95wil@gmail.com>
2021-03-03 13:58:47 +0100

    add my own show implementation

--- src/Simply/AST.hs
+++ src/Simply/AST.hs
@@ -10,11 +10,29 @@ data Term'Infer
   | Bound Int String
   | Free Name
   | Term'Infer :@: Term'Check
-  deriving (Show, Eq)
+  deriving (Eq)
+
+
+instance Show Term'Infer where
+  show (term ::: type')
+    = show term ++ " :: " ++ show type'
+  show (Bound ind name)
+    = name ++ show ind
+  show (Free name)
+    = show name
+  show (left :@: right)
+    = "(" ++ show left ++ " " ++ show right ++ ")"
 
 
 -- Checkable Term
 data Term'Check
   = Inf Term'Infer
   | Lam String Term'Check
-  deriving (Show, Eq)
+  deriving (Eq)
+
+
+instance Show Term'Check where
+  show (Inf term)
+    = show term
+  show (Lam par body)
+    = "(λ " ++ par ++ " -> " ++ show body ++ ")"
