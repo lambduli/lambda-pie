6d1367fd2f5c269cf627917ac5aac6538264a1ff
Taskkill <johny95wil@gmail.com>
2021-03-07 15:51:31 +0100

    implement own show method

--- src/Dependently/AST.hs
+++ src/Dependently/AST.hs
@@ -11,11 +11,34 @@ data Term'Infer
   | Bound Int String
   | Free Name
   | Term'Infer :@: Term'Check
-  deriving (Show, Eq)
+  | LamAnn String Term'Check Term'Infer
+  deriving (Eq)
+
+
+instance Show Term'Infer where
+  show (term ::: type')
+    = show term ++ " :: " ++ show type'
+  show (Bound ind name)
+    = name ++ show ind
+  show (Free name)
+    = show name
+  show (left :@: r@(Inf (r'l :@: r'r)))
+    = show left ++ " (" ++ show r ++ ")"
+  show (left :@: right)
+    = show left ++ " " ++ show right
+  show (LamAnn par in'type body)
+    = "(λ " ++ par ++ " :: " ++ show in'type ++ " -> " ++ show body ++ ")"
 
 
 -- Checkable Term
 data Term'Check
   = Inf Term'Infer
   | Lam String Term'Check
-  deriving (Show, Eq)
+  deriving (Eq)
+
+
+instance Show Term'Check where
+  show (Inf term)
+    = show term
+  show (Lam par body)
+    = "(λ " ++ par ++ " -> " ++ show body ++ ")"
