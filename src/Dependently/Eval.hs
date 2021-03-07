594e2c515aa61921a735ed23a51b976b6ea11e56
Taskkill <johny95wil@gmail.com>
2021-03-07 16:00:41 +0100

    fix evaluation and make the eval a class method

--- src/Dependently/Eval.hs
+++ src/Dependently/Eval.hs
@@ -13,11 +13,15 @@ eval'infer (Pi par in'type out'type) env
   = Val.Pi par (eval'check in'type env) out'type  env
 eval'infer (e ::: _) env
   = eval'check e env
+eval'infer (Bound ind name) env
+  = env !! ind
 eval'infer (Free name) env
   | Global id <- name = Val.Free id
   | Local _ id <- name = Val.Free id
 eval'infer (left :@: right) env
   = val'app (eval'infer left env) (eval'check right env)
+eval'infer (LamAnn par in'type body) env
+  = Val.Lam par (Inf body) env
 
 
 val'app :: Val.Value -> Val.Value -> Val.Value
@@ -34,3 +38,15 @@ eval'check (Inf e) env
   = eval'infer e env
 eval'check (Lam par body) env
   = Val.Lam par body env
+
+
+class Evaluate a where
+  eval :: a -> Val.Value
+
+
+instance Evaluate Term'Infer where
+  eval term = eval'infer term []
+
+
+instance Evaluate Term'Check where
+  eval term = eval'check term []
